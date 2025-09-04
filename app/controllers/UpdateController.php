29d9e4195e57b3c1581a636ee999ab9e4dd4e900
ikechukwu11 <kevinuzoma@gmail.com>
2025-09-04 05:01:29 +0100

    0.0.6

--- app/controllers/UpdateController.php
+++ app/controllers/UpdateController.php
@@ -4,18 +4,11 @@ namespace PopularSeach\Controllers;
 
 class UpdateController extends Controller
 {
-  protected $remoteUrl = 'https://raw.githubusercontent.com/Ikechukwu11/popular_search/master/update.json';
+  protected $repo = 'Ikechukwu11/popular_search'; // GitHub repo
   protected $pluginFile = 'popular-search/popular-search.php';
   protected $slug = 'popular-search';
 
-  // public function __construct()
-  // {
-  //   add_filter('site_transient_update_plugins', [$this, 'checkForUpdates']);
-  //   add_filter('plugins_api', [$this, 'pluginInfo'], 10, 3);
-  //   echo 'Yoooo';
-  // }
-
-    public static function hooks()
+  public static function hooks()
   {
     $controller = new self();
 
@@ -23,14 +16,31 @@ class UpdateController extends Controller
       [
         'hook' => 'site_transient_update_plugins',
         'callback' => [$controller, 'checkForUpdates'],
-        'type'=>'filter'
+        'type' => 'filter'
       ],
       [
         'hook' => 'plugins_api',
         'callback' => [$controller, 'pluginInfo'],
-        'type'=>'filter'
+        'type' => 'filter'
       ]
     ];
+  }
+
+  /**
+   * Fetch latest GitHub release
+   */
+  protected function fetchLatestRelease()
+  {
+    $url = "https://api.github.com/repos/{$this->repo}/releases/latest";
+    $response = wp_remote_get($url, [
+      'headers' => ['User-Agent' => 'WordPress']
+    ]);
+
+    if (is_wp_error($response) || wp_remote_retrieve_response_code($response) !== 200) {
+      return null;
+    }
+
+    return json_decode(wp_remote_retrieve_body($response));
   }
 
   public function checkForUpdates($transient)
@@ -39,22 +49,22 @@ class UpdateController extends Controller
       return $transient;
     }
 
-    $remote = wp_remote_get($this->remoteUrl);
+    $release = $this->fetchLatestRelease();
+    if (!$release || empty($release->tag_name)) {
+      return $transient;
+    }
 
-    if (!is_wp_error($remote) && wp_remote_retrieve_response_code($remote) == 200) {
-      $remote = json_decode(wp_remote_retrieve_body($remote));
+    $currentVersion = $transient->checked[$this->pluginFile] ?? '0.0.0';
+    $latestVersion  = ltrim($release->tag_name, 'v'); // strip "v" prefix if any
 
-      $currentVersion = $transient->checked[$this->pluginFile] ?? '0.0.0';
-
-      if ($remote && version_compare($currentVersion, $remote->version, '<')) {
-        $transient->response[$this->pluginFile] = (object)[
-          'slug'        => $this->slug,
-          'plugin'      => $this->pluginFile,
-          'new_version' => $remote->version,
-          'url'         => $remote->homepage,
-          'package'     => $remote->download_url,
-        ];
-      }
+    if (version_compare($currentVersion, $latestVersion, '<')) {
+      $transient->response[$this->pluginFile] = (object)[
+        'slug'        => $this->slug,
+        'plugin'      => $this->pluginFile,
+        'new_version' => $latestVersion,
+        'url'         => $release->html_url,
+        'package'     => $release->assets[0]->browser_download_url ?? $release->zipball_url,
+      ];
     }
 
     return $transient;
@@ -62,32 +72,25 @@ class UpdateController extends Controller
 
   public function pluginInfo($result, $action, $args)
   {
-    if ($action !== 'plugin_information') {
+    if ($action !== 'plugin_information' || $args->slug !== $this->slug) {
       return $result;
     }
 
-    if ($args->slug !== $this->slug) {
+    $release = $this->fetchLatestRelease();
+    if (!$release) {
       return $result;
     }
 
-    $remote = wp_remote_get($this->remoteUrl);
-
-    if (!is_wp_error($remote) && wp_remote_retrieve_response_code($remote) == 200) {
-      $remote = json_decode(wp_remote_retrieve_body($remote));
-
-      return (object)[
-        'name'          => $remote->name,
-        'slug'          => $remote->slug,
-        'version'       => $remote->version,
-        'author'        => $remote->author,
-        'homepage'      => $remote->homepage,
-        'download_link' => $remote->download_url,
-        'sections'      => [
-          'description' => 'Track search terms and counts.',
-        ],
-      ];
-    }
-
-    return $result;
+    return (object)[
+      'name'          => 'Popular Search',
+      'slug'          => $this->slug,
+      'version'       => ltrim($release->tag_name, 'v'),
+      'author'        => '<a href="https://github.com/Ikechukwu11">You</a>',
+      'homepage'      => $release->html_url,
+      'download_link' => $release->assets[0]->browser_download_url ?? $release->zipball_url,
+      'sections'      => [
+        'description' => $release->body ?? 'Track search terms and counts.',
+      ],
+    ];
   }
 }
--- popular-search.php
+++ popular-search.php
@@ -3,8 +3,9 @@
 /**
  * Plugin Name: Popular Search Tracker
  * Description: Track search terms and counts.
- * Version: 0.0.5
+ * Version: 0.0.6
  * Author: Ikechukwu11
+ * Author URI: https://github.com/Ikechukwu11
  */
 
 if (!defined('ABSPATH')) exit;
